8502da8ed6c4161d503efebff17adc988443bbd4
Artur Dolhii <artur.dolhii@medhost.com>
2020-02-04 13:12:38 +0200

    bootstrap angularjs from angular

--- angular-demo/src/app/app.component.ts
+++ angular-demo/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UpgradeModule} from '@angular/upgrade/static';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-demo';
+
+  constructor(private upgrade: UpgradeModule) {}
+
+  ngOnInit(): void {
+    this.upgrade.bootstrap(document.body, ['phonecatApp']);
+  }
 }
